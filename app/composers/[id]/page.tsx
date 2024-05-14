fbf63710313cb2f4d03ea50522b5ac33a0da84ef
Aidan Lao <laoaidan@gmail.com>
2024-05-13 22:11:30 -0300

    centered the composer pages

--- app/composers/[id]/page.tsx
+++ app/composers/[id]/page.tsx
@@ -7,7 +7,7 @@ export default async function Page({ params } : { params: { id: string }}) {
     return (
 
 
-        <div className="w-100 flex flex-col-reverse md:flex-row">
+        <div className="w-100 flex justify-center flex-col-reverse md:flex-row">
             {/* <p>ID: {params.id}</p>
             <p>sdf</p>
             <p>composer {post?.name}</p> */}
@@ -16,7 +16,7 @@ export default async function Page({ params } : { params: { id: string }}) {
             <ComposerImage url={composer?.image}/>
             
             </div>
-            <div className="md:basis-3/4 flex gap-4 pb-4 flex-col md:px-5">
+            <div className="md:basis-3/4 flex gap-4 pb-4 flex-col max-w-2xl md:px-5">
                 <h1 className={`text-2xl md:text-8xl font-black md:relative -left-10 z-10`}>{composer?.lastName}</h1>
                 <p className="text-xl font-thin">{composer?.name}</p>
                 <p className="max-w-2xl">{composer?.description}</p>
