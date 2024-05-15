1fc8542417c092d6f50dad7d2e46262696c0a0c8
Aidan Lao <laoaidan@gmail.com>
2024-05-14 21:05:07 -0300

    Removed search input from navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -32,27 +32,7 @@ import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
 	const [ isMenuOpen, setIsMenuOpen ] = useState(false);
-	const searchInput = (
-		<Input
-			aria-label="Search"
-			classNames={{
-				inputWrapper: "bg-default-100",
-				input: "text-sm",
-			}}
-			endContent={
-				<Kbd className="hidden lg:inline-block" keys={["command"]}>
-					K
-				</Kbd>
-			}
-			labelPlacement="outside"
-			placeholder="Search..."
-			startContent={
-				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-			}
-			type="search"
-		/>
-	);
-
+	
 	return (
 		<NextUINavbar
 		isMenuOpen={isMenuOpen}
@@ -98,7 +78,6 @@ export const Navbar = () => {
 			</NavbarContent>
 
 			<NavbarMenu>
-				{searchInput}
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 					{siteConfig.navMenuItems.map((item, index) => (
 						<NavbarMenuItem key={`${item}-${index}`}>
